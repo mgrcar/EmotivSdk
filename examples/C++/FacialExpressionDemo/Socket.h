aea3da8ef993dfbd08fc0d73362b07fb87d1c2fb
HoangPham <hoangpham@emotivcom>
2016-03-09 12:03:17 +0700

    Fixed some error display on C++ examples

--- examples/C++/FacialExpressionDemo/Socket.h
+++ examples/C++/FacialExpressionDemo/Socket.h
@@ -11,9 +11,8 @@
 #ifdef __linux__    
     #include <sys/types.h>
     #include <sys/socket.h>
+	#include <netinet/in.h>
 #endif
-
-#include <netinet/in.h>
 
 #ifdef __linux__
     typedef uint SOCKET;
--- examples/C++/MentalCommandDemo/Socket.h
+++ examples/C++/MentalCommandDemo/Socket.h
@@ -12,10 +12,8 @@
 #ifdef __linux__    
     #include <sys/types.h>
     #include <sys/socket.h>
-
+	#include <netinet/in.h>
 #endif
-
-#include <netinet/in.h>
 
 
 #ifdef __linux__
