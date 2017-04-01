b4e6d359f1dafe5cbbe5c850b501ea09c679f0a1
mgrcar <miha.grcar@ijs.si>
2017-04-01 19:57:32 +0200

    Added code to enable motion sensors if disabled

--- examples_basic/C#/MotionDataLogger/Program.cs
+++ examples_basic/C#/MotionDataLogger/Program.cs
@@ -31,6 +31,53 @@ namespace MotionDataLogger
             // record the user 
             userID = (int)e.userId;
 
+            uint EPOCmode, eegRate, eegRes, memsRate, memsRes;
+
+            engine.GetHeadsetSettings(
+                (uint)userID,
+                out EPOCmode,
+                out eegRate,
+                out eegRes,
+                out memsRate,
+                out memsRes
+            );
+
+            //\param EPOCmode
+            //--- If 0, then EPOC mode is EPOC.
+            //--- If 1, then EPOC mode is EPOC+.
+            //\param eegRate
+            //--- If 0, then EEG sample rate is 128Hz.
+            //--- If 1, then EEG sample rate is 256Hz.
+            //\param eegRes
+            //--- If 0, then EEG resolution is 14bit.
+            //--- If 1, then EEG resolution is 16bit.
+            //\param memsRate
+            //--- If 0, then motion sample rate is OFF.
+            //--- If 1, then motion sample rate is 32Hz.
+            //--- If 2, then motion sample rate is 64Hz.
+            //--- If 3, then motion sample rate is 128Hz.
+            //\param memsRes
+            //--- If 0, then motion resolution is 12bit.
+            //--- If 1, then motion resolution is 14bit.
+            //--- If 2, then motion resolution is 16bit.
+
+            Console.WriteLine("Current config: {0} {1} {2} {3} {4}", EPOCmode, eegRate, eegRes, memsRate, memsRes);
+
+            if (memsRate == 0) 
+            {
+                Console.WriteLine("*** The motion sensors are disabled. Will attempt to enable them. This will only work if the headset is connected via the USB cable. The application will exit after the reconfig attempt.");
+                engine.SetHeadsetSettings(
+                    (uint)userID,
+                    EPOCmode: EPOCmode,
+                    eegRate: eegRate,
+                    eegRes: eegRes,
+                    memsRate: 1,
+                    memsRes: memsRes
+                );
+                engine.Disconnect();
+                Environment.Exit(-1);
+            }
+
             // ask for up to 1 second of buffered data
             engine.MotionDataSetBufferSizeInSec(1);
         }
